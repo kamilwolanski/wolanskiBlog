81ecf666d5779c5ef1d9f1a65ca5bdae626e94e7
Kamil Wolański <kwolanski3@gmail.com>
2021-04-29 13:11:28 +0200

    added content to favicon query

--- src/Queries/basicQuery.ts
+++ src/Queries/basicQuery.ts
@@ -91,8 +91,9 @@ export class Queries {
       {
         _site {
           faviconMetaTags {
-            tag
             attributes
+            content
+            tag
           }
         }
       }
