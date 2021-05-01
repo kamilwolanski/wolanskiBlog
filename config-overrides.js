5a3e89c8a6b015f7d7b9f6d9de27fb9acf3565a2
Kamil Wolański <kwolanski3@gmail.com>
2021-05-01 15:50:40 +0200

    sdfvc

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,8 @@ const rewireStyledComponents = require('react-app-rewire-styled-components');
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
-  config = rewireStyledComponents(config, env);
+  config = rewireStyledComponents(config, env, {
+    ssr: true,
+  });
   return config;
 };
