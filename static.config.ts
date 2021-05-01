9dabfe0886e9036f88b1f371b9d17c7c3d104fad
Kamil Wolański <kwolanski3@gmail.com>
2021-05-01 14:50:26 +0200

    xcv

--- static.config.ts
+++ static.config.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  plugins: ['react-static-plugin-styled-components'],
-};
+// export default {
+//   plugins: ['react-static-plugin-styled-components'],
+// };
