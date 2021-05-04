e95e9caa4082c6c9a2d17b84c5aeecaf6b3436dd
Kamil Wolański <kwolanski3@gmail.com>
2021-05-04 10:24:36 +0200

    svcx

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './views/App';
 import { hydrate, render } from 'react-dom';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.hydrate(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const rootElement: any = document.getElementById('root');
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+
+// const rootElement: any = document.getElementById('root');
+// if (rootElement.hasChildNodes()) {
+//   hydrate(<App />, rootElement);
+// } else {
+//   render(<App />, rootElement);
+// }
