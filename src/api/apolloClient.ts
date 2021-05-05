25a551b053ef65c9784a487c1f4c4ba47ab736bc
Kamil Wolański <kwolanski3@gmail.com>
2021-05-05 11:42:25 +0200

    added env

--- src/api/apolloClient.ts
+++ src/api/apolloClient.ts
@@ -1,15 +1,16 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-const DATO_TOKEN = '189f78e078cae1db31f355940fef1b';
+// const DATO_TOKEN = '189f78e078cae1db31f355940fef1b';
 
 const httpLink = createHttpLink({
   uri: 'https://graphql.datocms.com',
 });
+console.log(process.env.REACT_APP_DATO_TOKEN);
 
 const authLink = setContext((_, { headers }) => {
   return {
-    headers: { ...headers, authorization: `Bearer ${DATO_TOKEN}` },
+    headers: { ...headers, authorization: `Bearer ${process.env.REACT_APP_DATO_TOKEN}` },
   };
 });
 
