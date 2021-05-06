64a235a13e9be44c81aeb7ad38cd2670832adee2
Kamil Wolański <kwolanski3@gmail.com>
2021-05-06 15:47:33 +0200

    added animation to heroSection

--- src/components/molecules/HeroSection/HeroSection.tsx
+++ src/components/molecules/HeroSection/HeroSection.tsx
@@ -1,11 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
 import { Wrapper, HeroContainer } from './HeroSection.styles';
 import { NavContext } from '../../../providers/NavProvider';
 const HeroSection = () => {
   const { isOpen } = useContext(NavContext);
 
+  const heroRef = useRef(null);
+  useEffect(() => {
+    gsap.from(heroRef.current, { duration: 1, opacity: 0 });
+  }, []);
+
   return (
-    <HeroContainer blur={isOpen ? true : false}>
+    <HeroContainer blur={isOpen ? true : false} ref={heroRef}>
       <Wrapper blur={isOpen ? true : false}>
         <div className="welcome-text">
           <h1>Witaj na moim blogu</h1>
